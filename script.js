bc794f3c4f9bc889da773b934cc749ef75cc2f92
Dinesh <80633667+Decode-dinesh@users.noreply.github.com>
2021-08-16 20:06:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(function timer1() {
+    document.getElementsByTagName("h1")[0].innerHTML = 10;
+    setTimeout(function timer1() {
+        document.getElementsByTagName("h1")[0].innerHTML = 9;
+        setTimeout(function timer1() {
+            document.getElementsByTagName("h1")[0].innerHTML = 8;
+            setTimeout(function timer1() {
+                document.getElementsByTagName("h1")[0].innerHTML = 7;
+                setTimeout(function timer1() {
+                    document.getElementsByTagName("h1")[0].innerHTML = 6;
+                    setTimeout(function timer1() {
+                        document.getElementsByTagName("h1")[0].innerHTML = 5;
+                        setTimeout(function timer1() {
+                            document.getElementsByTagName("h1")[0].innerHTML = 4;
+                            setTimeout(function timer1() {
+                                document.getElementsByTagName("h1")[0].innerHTML = 3;
+                                setTimeout(function timer1() {
+                                    document.getElementsByTagName("h1")[0].innerHTML = 2;
+                                    setTimeout(function timer1() {
+                                        document.getElementsByTagName("h1")[0].innerHTML = 1;
+                                        setTimeout(function timer1() {
+                                            document.getElementsByTagName("h1")[0].innerHTML = "Happy Independence day";
+
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
