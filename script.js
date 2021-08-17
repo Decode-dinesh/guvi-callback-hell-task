1869433fbbd69aa3c28856b32c677c60fda013ff
Dinesh <80633667+Decode-dinesh@users.noreply.github.com>
2021-08-17 18:47:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-setTimeout(function timer1() {
+setTimeout(() => {
     document.getElementsByTagName("h1")[0].innerHTML = 10;
-    setTimeout(function timer1() {
+    setTimeout(() => {
         document.getElementsByTagName("h1")[0].innerHTML = 9;
-        setTimeout(function timer1() {
+        setTimeout(() => {
             document.getElementsByTagName("h1")[0].innerHTML = 8;
-            setTimeout(function timer1() {
+            setTimeout(() => {
                 document.getElementsByTagName("h1")[0].innerHTML = 7;
-                setTimeout(function timer1() {
+                setTimeout(() => {
                     document.getElementsByTagName("h1")[0].innerHTML = 6;
-                    setTimeout(function timer1() {
+                    setTimeout(() => {
                         document.getElementsByTagName("h1")[0].innerHTML = 5;
-                        setTimeout(function timer1() {
+                        setTimeout(() => {
                             document.getElementsByTagName("h1")[0].innerHTML = 4;
-                            setTimeout(function timer1() {
+                            setTimeout(() => {
                                 document.getElementsByTagName("h1")[0].innerHTML = 3;
-                                setTimeout(function timer1() {
+                                setTimeout(() => {
                                     document.getElementsByTagName("h1")[0].innerHTML = 2;
-                                    setTimeout(function timer1() {
+                                    setTimeout(() => {
                                         document.getElementsByTagName("h1")[0].innerHTML = 1;
-                                        setTimeout(function timer1() {
-                                            document.getElementsByTagName("h1")[0].innerHTML = "Happy Independence day";
-
+                                        setTimeout(() => {
+                                            document.getElementsByTagName("h1")[0].innerHTML = "Happy Independance Day";
                                         }, 1000)
                                     }, 1000)
                                 }, 1000)
